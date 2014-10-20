920716eda45600879a0bb1fd642f982e9483ad35
Tim Brooks <brooks@cern.ch>
2014-10-20 19:48:16 +0100

    Added .vim/bundle/update.sh from pi2

---
+++ home/.vim/bundle/update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "#"$(date) > update.list
+for repo in $(find * -maxdepth 0 -type d); do
+    cd $repo
+    url=""
+    for remote in $(git remote -v | grep fetch | awk '{print $1}'); do
+        url=$(git remote -v | grep -e "^${remote}" | grep fetch | awk '{print $2}')
+        echo "${repo}: ${url}"
+        if [ "$remote" == "origin" ]; then
+            git pull ${remote} master
+        fi
+    done
+    cd ..
+    echo ${url} >> update.list
+done
+
+vim -RET dumb -c 'Helptags' -c 'q' > /dev/null
