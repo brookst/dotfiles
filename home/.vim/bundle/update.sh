88fb5acd1479b8ec63ee598cbc445060c7fba67f
Tim Brooks <brooks@cern.ch>
2016-01-12 20:04:51 +0000

    File update @tims-laptop

--- home/.vim/bundle/update.sh
+++ home/.vim/bundle/update.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-echo "#"$(date) > update.list
+echo "#$(date)" > update.list
 for repo in $(find * -maxdepth 0 -type d); do
-    cd $repo
+    cd "$repo"
     url=""
     for remote in $(git remote -v | grep fetch | awk '{print $1}'); do
         url=$(git remote -v | grep -e "^${remote}" | grep fetch | awk '{print $2}')
         echo "${repo}: ${url}"
         if [ "$remote" == "origin" ]; then
-            git pull ${remote} master
+            git pull "${remote}" master
+        else
+            echo "Not updateing from remote ${remote}"
         fi
     done
     cd ..
-    echo ${url} >> update.list
+    echo "${url}" >> update.list
 done
 
 vim -RET dumb -c 'Helptags' -c 'q' > /dev/null
